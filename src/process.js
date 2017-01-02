c7f520b5051c9957e388cbfbd1f23615ddecfab8
Matt Travi <programmer@travi.org>
2017-01-02 15:41:08 -0600

    WIP(process): posted a comment to the PR when one of the processing steps fails

--- src/process.js
+++ src/process.js
@@ -2,8 +2,9 @@ import createActions from './github/actions';
 
 export default function ({payload}, {github, squash, deleteBranches}) {
   const {pull_request, number} = payload;
-  const {acceptPR, deleteBranch} = createActions(github);
+  const {acceptPR, deleteBranch, postErrorComment} = createActions(github);
 
   return acceptPR(pull_request.url, pull_request.head.sha, number, squash)
-    .then(() => deleteBranch(pull_request.head, deleteBranches));
+    .then(() => deleteBranch(pull_request.head, deleteBranches))
+    .catch((err) => postErrorComment(pull_request.url, err));
 }
--- test/unit/process-test.js
+++ test/unit/process-test.js
@@ -5,27 +5,32 @@ import processPR from '../../src/process';
 import * as actionsFactory from '../../src/github/actions';
 
 suite('process', () => {
-  let sandbox, acceptPR, deleteBranch;
+  let sandbox, acceptPR, deleteBranch, postErrorComment;
   const githubCredentials = any.simpleObject();
+  const url = any.url();
+  const sha = any.string();
+  const head = {sha};
+  const number = any.integer();
+  const squash = any.boolean();
+  const deleteBranches = any.boolean();
 
   setup(() => {
     sandbox = sinon.sandbox.create();
 
     acceptPR = sinon.stub();
     deleteBranch = sinon.stub();
+    postErrorComment = sinon.stub();
 
-    sandbox.stub(actionsFactory, 'default').withArgs(githubCredentials).returns({acceptPR, deleteBranch});
+    sandbox.stub(actionsFactory, 'default').withArgs(githubCredentials).returns({
+      acceptPR,
+      deleteBranch,
+      postErrorComment
+    });
   });
 
   teardown(() => sandbox.restore());
 
   test('that processing a greenkeeper PR confirms that it can be merged, merges, and deletes the branch', () => {
-    const url = any.url();
-    const sha = any.string();
-    const head = {sha};
-    const number = any.integer();
-    const squash = any.boolean();
-    const deleteBranches = any.boolean();
     acceptPR.withArgs(url, sha, number, squash).resolves();
     deleteBranch.resolves();
 
@@ -36,5 +41,18 @@ suite('process', () => {
       assert.calledOnce(acceptPR);
       assert.calledWith(deleteBranch, head, deleteBranches);
     });
+  });
+
+  test('that failing to merge the PR causes a comment to be logged against the PR and prevents branch deletion', () => {
+    const error = new Error(any.string());
+    acceptPR.rejects(error);
+
+    return processPR(
+      {payload: {number, pull_request: {url, head}}},
+      {github: githubCredentials, squash, deleteBranches}
+    ).then(() => {
+      assert.notCalled(deleteBranch);
+      assert.calledWith(postErrorComment, url, error);
+    })
   });
 });
