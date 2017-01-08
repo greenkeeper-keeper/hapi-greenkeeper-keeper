57905282b0f44b5239a15b673c8dabaf31c7786b
Matt Travi <programmer@travi.org>
2017-01-08 11:41:39 -0600

    fix(error): included the error class that i forgot to stage for branch deletion problems

---
+++ src/branch-deletion-failure-error.js
@@ -0,0 +1,6 @@
+export default class BranchDeletionFailureError extends Error {
+  constructor (message) {
+    super(`An attempt to delete this branch failed. ${message}`);
+    this.name = 'BranchDeletionFailureError';
+  }
+}
