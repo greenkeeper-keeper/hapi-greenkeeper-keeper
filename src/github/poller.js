46fc7df414cdbfe72214909115cfa6d73aa29b9a
Matt Travi <programmer@travi.org>
2017-01-08 11:40:42 -0600

    feat(error): rejected a pending status timeout with an informative error

--- src/github/poller.js
+++ src/github/poller.js
@@ -1,8 +1,9 @@
 import delay from 'delay';
 import {hours} from 'milliseconds';
+import PendingTimeoutError from '../pending-timeout-error';
 
 export default function (options, timeout, callback) {
-  if (timeout > hours(1)) return Promise.reject();
+  if (timeout > hours(1)) return Promise.reject(new PendingTimeoutError());
 
   return delay(timeout).then(() => callback(options, timeout * 2));
 }
--- test/unit/github/poller-test.js
+++ test/unit/github/poller-test.js
@@ -2,9 +2,10 @@ import sinon from 'sinon';
 import {assert} from 'chai';
 import any from '@travi/any';
 import proxyquire from 'proxyquire';
+import PendingTimeoutError from '../../../src/pending-timeout-error';
 
-const MINUTE = 1000 * 60
-const HOUR = MINUTE * 60
+const MINUTE = 1000 * 60;
+const HOUR = MINUTE * 60;
 
 suite('poller', () => {
   const delay = sinon.stub();
@@ -14,13 +15,15 @@ suite('poller', () => {
     const options = any.simpleObject();
     const timeout = any.integer({max: HOUR});
     const callback = sinon.stub();
-    callback.withArgs(options, timeout * 2).resolves();
+    callback.resolves();
     delay.withArgs(timeout).resolves();
 
-    return assert.isFulfilled(poll(options, timeout, callback));
+    return poll(options, timeout, callback).then(() => {
+      assert.calledWith(callback, options, timeout * 2);
+    });
   });
 
   test('that the poller rejects if the timeout is beyond an hour', () => {
-    return assert.isRejected(poll({}, HOUR + 1));
+    return assert.isRejected(poll({}, HOUR + 1), PendingTimeoutError, /Pending statuses timeout/);
   });
 });
