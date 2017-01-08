5494286cf61d858f851883ee8f4fe6c370909239
Matt Travi <programmer@travi.org>
2017-01-08 14:36:59 -0600

    build(rollup): worked around rollups lack of using the node logic for resolving modules
    since i now use an index for the errors, the node lookup logic is necessary over rollup's default
    
    strategy

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'src/plugin.js',
+  external: [
+    'http-status-codes',
+    'milliseconds',
+    'highwire',
+    'delay'
+  ],
   plugins: [
+    nodeResolve({
+      module: true,
+      jsnext: true
+    }),
     babel({
       babelrc: false,
       exclude: ['./node_modules/**'],
