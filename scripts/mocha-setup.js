34438126cc531c1142e17c22011b4021aa5a6e3c
Matt Travi <programmer@travi.org>
2017-03-21 20:20:52 -0500

    refactor(sinon-as-promised): removed sinon-as-promised
    this functionality is now build directly into sinon v2

--- scripts/mocha-setup.js
+++ scripts/mocha-setup.js
@@ -3,15 +3,5 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 
-sinon.behavior = require('sinon/lib/sinon/behavior');
-
-sinon.defaultConfig = {
-  injectInto: null,
-  properties: ['spy', 'stub', 'mock', 'clock', 'server', 'requests'],
-  useFakeTimers: true,
-  useFakeServer: true
-};
-require('sinon-as-promised');
-
 chai.use(chaiAsPromised);
 sinon.assert.expose(chai.assert, {prefix: ''});
