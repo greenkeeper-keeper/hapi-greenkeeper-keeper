6460fef4993fc7879f41121613681819e4c8dab2
Matt Travi <matt@gaincompliance.com>
2017-05-23 10:12:15 -0500

    fix(greenkeeper-bot): updated user check to align to new app url
    it looks like the github marketplace announcement resulted in integrations getting a url change to
    
    sit under /app rather than the previous /integration
    
    fixes #151

--- test/unit/greenkeeper-test.js
+++ test/unit/greenkeeper-test.js
@@ -7,7 +7,7 @@ import openedByGreenkeeperBot, {
 suite('greenkeeper', () => {
   test('that the users are defined to match the greenkeeper bots', () => {
     assert.equal(GREENKEEPER_BOT_GITHUB_URL, 'https://github.com/greenkeeperio-bot');
-    assert.equal(GREENKEEPER_INTEGRATION_GITHUB_URL, 'https://github.com/integration/greenkeeper');
+    assert.equal(GREENKEEPER_INTEGRATION_GITHUB_URL, 'https://github.com/app/greenkeeper');
   });
 
   test('that true is returned for the bot used for the legacy bot', () => {
