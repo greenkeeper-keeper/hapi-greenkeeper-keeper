59e153cb39e7cbfa45a6224be411f60d34f3979b
Matt Travi <programmer@travi.org>
2017-05-29 08:20:50 -0500

    fix(greenkeeper-user): fixed the url for the updated greenkeeper location
    for #151

--- src/greenkeeper.js
+++ src/greenkeeper.js
@@ -1,5 +1,5 @@
 export const GREENKEEPER_BOT_GITHUB_URL = 'https://github.com/greenkeeperio-bot';
-export const GREENKEEPER_INTEGRATION_GITHUB_URL = 'https://github.com/app/greenkeeper';
+export const GREENKEEPER_INTEGRATION_GITHUB_URL = 'https://github.com/apps/greenkeeper';
 
 export default function (sender) {
   return GREENKEEPER_INTEGRATION_GITHUB_URL === sender || GREENKEEPER_BOT_GITHUB_URL === sender;
